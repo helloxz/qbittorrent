239e3e37bba0225c6321be833c93631f552c57f6
xiaoz <xiaoz93@outlook.com>
2021-03-19 12:57:31 +0800

    fix bug

--- run.sh
+++ run.sh
@@ -6,10 +6,10 @@ if [ -f "/etc/qBittorrent/config/qBittorrent.conf.bak" ];then
 	echo -e "y" | qbittorrent-nox --profile=/etc
 else
 	echo -e "y" | qbittorrent-nox --profile=/etc
-	curl http://127.0.0.1:18080/
+	curl http://127.0.0.1:8080/
 	sleep 20
 	pkill -9 qbittorrent-nox
-	mv /etc/qBittorrent/config/qBittorrent.conf /etc/qBittorrent/config/qBittorrent.conf.bak
+	#mv /etc/qBittorrent/config/qBittorrent.conf /etc/qBittorrent/config/qBittorrent.conf.bak
 	curl -o /etc/qBittorrent/config/qBittorrent.conf https://raw.githubusercontent.com/helloxz/qbittorrent/main/qBittorrent.conf
 	echo -e "y" | qbittorrent-nox --profile=/etc
 	tail -100f /etc/qBittorrent/data/logs/qbittorrent.log
