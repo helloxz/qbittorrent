c837795e6a46752dc2bba1b6a8e2b7f0ae9f9165
xiaoz <xiaoz93@outlook.com>
2021-03-19 21:57:41 +0800

    fix bug

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ else
 	curl -kLo /etc/qBittorrent/config.tar.gz https://github.com/helloxz/qbittorrent/raw/main/config.tar.gz
 	cd /etc/qBittorrent/ && tar -xvf /etc/qBittorrent/config.tar.gz
 	rm -rf /etc/qBittorrent/config.tar.gz
-	mv /root/GeoLite2-Country.mmdb /etc/qBittorrent/data/GeoIP
+	mv /root/GeoLite2-Country.mmdb /etc/qBittorrent/data/GeoIP/GeoLite2-Country.mmdb
 	echo -e "y" | qbittorrent-nox --profile=/etc
 	tail -100f /etc/qBittorrent/data/logs/qbittorrent.log
 fi
