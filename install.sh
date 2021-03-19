d038fdc4c1fd503128f1bbba079a8805322e1280
xiaoz <xiaoz93@outlook.com>
2021-03-19 21:46:25 +0800

    fix bug

--- install.sh
+++ install.sh
@@ -13,6 +13,6 @@ mkdir -p /etc/qBittorrent/data/GeoIP
 #创建下载目录
 mkdir -p /downloads
 #下载GeoIP数据库
-curl -kLo /etc/qBittorrent/data/GeoIP/GeoLite2-Country.mmdb https://github.com/helloxz/qbittorrent/raw/main/GeoLite2-Country.mmdb
+curl -kLo /root/GeoLite2-Country.mmdb https://github.com/helloxz/qbittorrent/raw/main/GeoLite2-Country.mmdb
 
 chmod +x /usr/sbin/run.sh
